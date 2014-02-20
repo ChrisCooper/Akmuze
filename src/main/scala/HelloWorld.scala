54625262271c296fe91c7c49e19e128862171232
Chris Cooper <chriscooper1991@gmail.com>
2014-02-20 05:45:17 -0500

    Moved table defs to model file

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -2,30 +2,11 @@ package com.quixoticchris.Akmuze
 
 // Use SQLiteDriver to connect to a SQLite database
 import scala.slick.driver.SQLiteDriver.simple._
+import com.quixoticchris.Akmuze.Model._
 
 object Hello extends App {
+	println("------")
 	println("Hey! Running a cool SQLite test.")
-	
-	// Definition of the SONGS table
-	class Composer(tag: Tag) extends Table[(Int, String)](tag, "COMPOSERS") {
-		def id = column[Int]("COMPOSER_ID", O.PrimaryKey)
-		def name = column[String]("NAME")
-		
-		// Every table needs a * projection with the same type as the table's type parameter
-		def * = (id, name)
-	}
-	val composers = TableQuery[Composer]
-	
-	// Definition of the SONGS table
-	class Song(tag: Tag) extends Table[(Int, String, Int)](tag, "SONGS") {
-		def id = column[Int]("SONG_ID", O.PrimaryKey)
-		def title = column[String]("TITLE")
-		def composerID = column[Int]("COMPOSER_ID")
-		
-		// Every table needs a * projection with the same type as the table's type parameter
-		def * = (id, title, composerID)
-	}
-	val songs = TableQuery[Song]
 	
 	// Connect to the database and execute the following block within a session
 	Database.forURL("jdbc:sqlite:experiment.db", driver = "org.sqlite.JDBC") withSession {
